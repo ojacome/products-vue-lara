5e72d83e93f7d2271f2691fd26edf66a3607967a
olmedo <olmedo,bdp@gmail.com>
2021-02-05 18:12:11 -0500

    migrations and seeder

---
+++ database/migrations/2021_02_05_172842_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);            
+            $table->integer('stock');            
+            $table->float('price');            
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');        
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Category::factory(10)->create();
+        $categories = Category::factory()->count(10)->create();
+
+        $categories->each( function( $category ){
+                        
+            $products = Product::factory()->count(5)->make();    
+            $category->products()->saveMany($products);
+        });
     }
 }
